3aee2958e917893cd29629d35b659d199814d6cf
Matthias Mohr <m.mohr@uni-muenster.de>
2021-08-10 11:32:50 +0200

    Fully use the ProcessRegistry from js-commons

--- src/registry.js
+++ src/registry.js
@@ -1,6 +1,4 @@
-const Process = require('./process');
 const CommonProcessRegistry = require('@openeo/js-commons/src/processRegistry');
-const Utils = require('./utils');
 
 /**
  * Central registry for processes.
@@ -13,29 +11,6 @@ const Utils = require('./utils');
  * @class
  */
 class ProcessRegistry extends CommonProcessRegistry {
-
-	add(process, namespace = 'backend') {
-		if (!Utils.isObject(process)) {
-			throw new Error("Invalid process; not an object.");
-		}
-
-		let isImpl = process instanceof Process;
-		if (!isImpl && typeof process.toJSON === 'function') {
-			var json = process.toJSON();
-			if (Utils.isObject(json)) {
-				process = json;
-			}
-		}
-		if (typeof process.id !== 'string') {
-			throw new Error("Invalid process; no id specified.");
-		}
-
-		super.add(isImpl ? process : new Process(process), namespace);
-	}
-
-	toJSON() {
-		return this.all().map(impl => impl.toJSON());
-	}
 
 }
 
